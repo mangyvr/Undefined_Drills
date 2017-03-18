35952c6c20b757b82df05d979488551f92689010
Billy Hung <billy7907@gmail.com>
2017-03-18 15:21:57 -0700

    Add userdrill joint table and dummy data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170318220627) do
+ActiveRecord::Schema.define(version: 20170318221729) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,17 @@ ActiveRecord::Schema.define(version: 20170318220627) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "user_drills", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "drill_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean  "completed"
+    t.integer  "attempts"
+    t.index ["drill_id"], name: "index_user_drills_on_drill_id", using: :btree
+    t.index ["user_id"], name: "index_user_drills_on_user_id", using: :btree
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
@@ -65,4 +76,6 @@ ActiveRecord::Schema.define(version: 20170318220627) do
   add_foreign_key "answers", "drills"
   add_foreign_key "drills", "groups"
   add_foreign_key "drills", "users"
+  add_foreign_key "user_drills", "drills"
+  add_foreign_key "user_drills", "users"
 end
