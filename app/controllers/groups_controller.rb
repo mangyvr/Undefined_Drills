2a8541e86d0a78d90c2595107dfc1638936f4b0a
Billy Hung <billy7907@gmail.com>
2017-03-18 15:07:53 -0700

    remove completed column from drill table

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -37,7 +37,7 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    
+
   end
 
   def update
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20170318184911) do
-# ActiveRecord::Schema.define(version: 20170318173013) do
-
+ActiveRecord::Schema.define(version: 20170318220627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,7 +32,6 @@ ActiveRecord::Schema.define(version: 20170318184911) do
     t.datetime "updated_at",  null: false
     t.integer  "group_id"
     t.integer  "user_id"
-    t.integer  "completed"
     t.index ["group_id"], name: "index_drills_on_group_id", using: :btree
     t.index ["user_id"], name: "index_drills_on_user_id", using: :btree
   end
