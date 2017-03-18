0254d9c13fbf603e316152ef11f16f37a68a8a9b
bernulander <bernulander@gmail.com>
2017-03-18 16:30:34 -0700

    changed form color

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
-ActiveRecord::Schema.define(version: 20170318194131) do
+ActiveRecord::Schema.define(version: 20170318223137) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,7 +32,6 @@ ActiveRecord::Schema.define(version: 20170318194131) do
     t.datetime "updated_at",  null: false
     t.integer  "group_id"
     t.integer  "user_id"
-    t.integer  "completed"
     t.index ["group_id"], name: "index_drills_on_group_id", using: :btree
     t.index ["user_id"], name: "index_drills_on_user_id", using: :btree
   end
