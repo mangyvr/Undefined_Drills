27ef2337fc36a57baaa65fd0b406f2b5717cc7e9
Tsung-Yen Tsai <tsung-yen.tsai@mail.mcgill.ca>
2017-03-18 10:44:28 -0700

    Commiting for integration merge

--- app/controllers/reset_password_controller.rb
+++ app/controllers/reset_password_controller.rb
@@ -3,7 +3,7 @@ class ResetPasswordController < ApplicationController
   end
 
   def create
-    @user = User.find params[:user_id]
+    @user = User.find_by(email: params[:reset_password][:email].downcase)
 
     if @user
       token = User.new_token
@@ -11,7 +11,7 @@ class ResetPasswordController < ApplicationController
       # For now display token and url on terminal
       p @user.gen_reset_link(request.base_url, token)
 
-      @user.reset_digest = User.hash_token(token)
+      @user.password_reset_token = User.hash_token(token)
       @user.reset_sent_at = Time.zone.now
 
       if @user.save
@@ -30,7 +30,7 @@ class ResetPasswordController < ApplicationController
   def update
     permitted_params = params.require(:reset_password).permit(:password, :password_confirmation, :email)
 
-    @user = User.find params[:user_id]
+    @user = User.find_by(email: params[:reset_password][:email].downcase)
 
     if @user
       if params[:reset_password][:password] == params[:reset_password][:password_confirmation]
@@ -39,16 +39,16 @@ class ResetPasswordController < ApplicationController
         # below if statement is same as:
         # unless BCrypt::Password.new(@user.reset_digest).is_password?(remember_token)
 
-        if BCrypt::Password.new(@user.reset_digest) != params[:id]
+        if BCrypt::Password.new(@user.password_reset_token) != params[:id]
           redirect_to posts_path, notice: "Link is invalid."
         elsif (@user.reset_sent_at + 3.days) < Time.now
           # Token has expired -- destroy it
-          @user.update_attribute("reset_digest", '')
+          @user.update_attribute("password_reset_token", '')
           redirect_to posts_path, notice: "Link is expired."
         else
           if @user.update permitted_params
           # if @user.update()
-            @user.update_attribute("reset_digest", '')
+            @user.update_attribute("password_reset_token", '')
             redirect_to posts_path, notice: "Password changed."
           else
             redirect_to posts_path, notice: "Password changed failed."
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [:new, :create] do
-    resources :reset_password, only: [:new, :create, :edit, :update]
-  end
+  resources :users, only: [:new, :create]
+
+  # Reset password must be independent of users -- no associated user yet
+  resources :reset_password, only: [:new, :create, :edit, :update]
 
   resources :sessions, only: [:new, :create] do
     delete :destroy, on: :collection
