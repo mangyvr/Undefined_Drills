d3b69aad11d5f9d0c6c5ba64aa84c69927b2451f
Jesse Falconer <jfalconer3.14@gmail.com>
2017-03-18 15:48:12 -0700

    added bookmarks

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,6 @@
 class GroupsController < ApplicationController
   before_action :get_group_id, except: [:index, :new, :create]
+  before_action :get_bookmarks, only: [:index]
 
 
   def index
@@ -29,7 +30,7 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    
+
   end
 
   def update
@@ -55,6 +56,10 @@ class GroupsController < ApplicationController
 
 
   private
+
+  def get_bookmarks
+    @usergroups = UserGroups.find_by_user_id session[:user_id]
+  end
 
   def get_group_id
     @group = Group.find params[:id]
