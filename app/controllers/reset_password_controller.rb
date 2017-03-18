0c12398f585444d5806fbe931e5ccfa62b589c0d
Tsung-Yen Tsai <tsung-yen.tsai@mail.mcgill.ca>
2017-03-17 20:48:42 -0700

    Added initial reset password controller -- awaiting emailer and testing

---
+++ app/controllers/reset_password_controller.rb
@@ -0,0 +1,67 @@
+class ResetPasswordController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find params[:user_id]
+
+    if @user
+      token = User.new_token
+
+      # For now display token and url on terminal
+      p @user.gen_reset_link(request.base_url, token)
+
+      @user.reset_digest = User.hash_token(token)
+      @user.reset_sent_at = Time.zone.now
+
+      if @user.save
+        redirect_to posts_path, notice: "Password reset email sent."
+      else
+        render :new, notice: "Reset failed."
+      end
+    else
+      render :new, notice: "Invalid email."
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    permitted_params = params.require(:reset_password).permit(:password, :password_confirmation, :email)
+
+    @user = User.find params[:user_id]
+
+    if @user
+      if params[:reset_password][:password] == params[:reset_password][:password_confirmation]
+
+        # Check if password token is too old or if it does not match digest
+        # below if statement is same as:
+        # unless BCrypt::Password.new(@user.reset_digest).is_password?(remember_token)
+
+        if BCrypt::Password.new(@user.reset_digest) != params[:id]
+          redirect_to posts_path, notice: "Link is invalid."
+        elsif (@user.reset_sent_at + 3.days) < Time.now
+          # Token has expired -- destroy it
+          @user.update_attribute("reset_digest", '')
+          redirect_to posts_path, notice: "Link is expired."
+        else
+          if @user.update permitted_params
+          # if @user.update()
+            @user.update_attribute("reset_digest", '')
+            redirect_to posts_path, notice: "Password changed."
+          else
+            redirect_to posts_path, notice: "Password changed failed."
+          end
+        end
+
+      else
+        flash.now[:alert] = "Passwords do not match."
+        render :edit
+      end
+    else
+      redirect_to root_path, notice: "Invalid link."
+    end
+  end
+
+end
