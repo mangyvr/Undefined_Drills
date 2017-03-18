16a47d6996d507c20fbed4fa2592b2724f2959ca
Andrew Krisjanis Dambenieks <drakdambenieks@gmail.com>
2017-03-17 18:52:32 -0700

    Created basic routes and controllers for groups, drills and answers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :groups, shallow: true do
+    resources :drills, shallow: true do
+      resources :answers, shallow: true
+    end
+  end
 end
