4a455acb800f83e7fb31699807cd7ff7f68320a3
Andrew Krisjanis Dambenieks <drakdambenieks@gmail.com>
2017-03-18 16:38:21 -0700

    Fixing Migrations and Seed - after pull rails db:drop, then rails db:create db:migrate db:seed

--- db/migrate/20170318223137_add_dummy_data_to_usergroup.rb
+++
@@ -1,7 +0,0 @@
-class AddDummyDataToUsergroup < ActiveRecord::Migration[5.0]
-  def change
-    10.times do
-      UserGroup.create(user_id: Faker::Number.between(1,10), group_id: Faker::Number.between(1,5))
-    end
-  end
-end
