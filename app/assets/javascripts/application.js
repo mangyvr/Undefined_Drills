7f4dfbcfd28f1ffe8b30b094623d4a544044260e
Sho <sfaizi1@outlook.com>
2017-03-19 11:26:17 -0700

    remove console.logs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,17 +30,12 @@ $(document).ready(function() {
 
   $('.allDrills').on('click', function() {
     $('.big-well').show();
-    console.log('Showing');
     $('.userDrills').hide();
-    console.log('Hiding');
   });
 
   $('.myDrills').on('click', function() {
-    // $('.drills-container').hide();
     $('.big-well').hide();
-    console.log('Hiding');
     $('.userDrills').show();
-    console.log('Showing');
   });
 
   var url = window.location;
